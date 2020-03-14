75272a474b2b014969e050daa7ba6742eb185707
Ian Esling <ian.esling@gmail.com>
2020-03-14 17:43:25 +0000

    fixed maven test runner

--- src/main/java/model/Items.java
+++ src/main/java/model/Items.java
@@ -3,10 +3,10 @@ package model;
 import java.util.*;
 
 public class Items {
-    public static final Item SOUP = Item.item("soup", 0.65d);
-    public static final Item BREAD = Item.item("bread", 0.80d);
-    public static final Item MILK = Item.item("milk", 1.30d);
-    public static final Item APPLES = Item.item("apples", 0.10d);
+    public static final Item SOUP = Item.item("soup", 0.65d, "tin");
+    public static final Item BREAD = Item.item("bread", 0.80d, "loaf");
+    public static final Item MILK = Item.item("milk", 1.30d, "bottle");
+    public static final Item APPLES = Item.item("apples", 0.10d, "single");
     private final Set<Item> items;
 
     private Items(Set<Item> items) {
